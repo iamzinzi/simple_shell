372a8bdfeaac5b46eb7410547609b36688139131
Jinji Zhang <362@holbertonschool.com>
2018-09-04 18:27:37 +0000

    Fix: move error messages to stderr form stdout

--- error_helper.c
+++ error_helper.c
@@ -19,12 +19,12 @@
 
 void error_helper(char **av, char **argv, char *to_string)
 {
-	write(STDOUT_FILENO, av[0], _strlen(av[0]));
-	write(STDOUT_FILENO, ": ", 2);
-	write(STDOUT_FILENO, to_string, _strlen(to_string));
-	write(STDOUT_FILENO, ": ", 2);
-	write(STDOUT_FILENO, argv[0], _strlen(argv[0]));
-	write(STDOUT_FILENO, ": not found\n", 12);
+	write(STDERR_FILENO, av[0], _strlen(av[0]));
+	write(STDERR_FILENO, ": ", 2);
+	write(STDERR_FILENO, to_string, _strlen(to_string));
+	write(STDERR_FILENO, ": ", 2);
+	write(STDERR_FILENO, argv[0], _strlen(argv[0]));
+	write(STDERR_FILENO, ": not found\n", 12);
 }
 
 /**
